43c070482612174e82886c16ea721fe84af9125f
oliver-lister <oliverlister98@gmail.com>
2024-12-11 16:32:41 +1100

    initalize vite react project with vitest sass axios

---
+++ test/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+import { describe, expect, it } from "vitest";
+import React from "react";
+
+describe("App", () => {
+  it("renders the App component", () => {
+    render(<App />);
+
+    const messages = screen.queryAllByText(/Vite/i);
+    expect(messages).toHaveLength(2); // Check the number of matches
+    expect(messages[0]).toBeVisible(); // Test the first match
+  });
+});
